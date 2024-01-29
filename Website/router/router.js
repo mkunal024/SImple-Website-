83505b5372feeb6172f6328b922e35e37acdd2cf
Kunal . <kunal.bioinfo.jvc@gmail.com>
2024-01-29 16:27:33 +0530

    added nested query

--- Website/router/router.js
+++ Website/router/router.js
@@ -16,16 +16,19 @@ router.get("/cities",(req,resp)=>{
 
 router.post("/filter-cities", (req, res) => {
     const selectedCountry = req.body.country; // Assuming you use body-parser for form parsing
-
+            
+    connection.query("SELECT distinct(CountryCode) FROM city;", (error, results) => {
+    
     // Query the database based on the selected country
     connection.query("SELECT * FROM city WHERE CountryCode = ?", [selectedCountry], (err, data) => {
         if (err) {
             res.status(500).send("Error querying the database: " + JSON.stringify(err));
         } else {
             // Render the home page with the filtered cities data and selected country
-            res.render('cities', { citiesData: data, selectedCountry });
+            res.render('home', { data: results,citiesData: data, selectedCountry });
         }
     });
+});
 });
 
 router.get('/', (req, res) => {
@@ -39,8 +42,5 @@ router.get('/', (req, res) => {
         res.render('home', { data: results });
     });
 });
-
-
-
 
 module.exports=router;
